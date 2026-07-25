55178a513f55c9c2e27f06158286244c354b24cf
unknown <ryan.cabas01@gmail.com>
2026-07-25 11:00:04 +0800

    feat: wire RecentRecordsPanel into app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { SyncProvider } from "@/components/sync/SyncProvider";
 import { DataProvider } from "@/context/DataProvider";
 import { DataLoader } from "@/components/common/DataLoader";
 import { ClientRipple } from "@/components/common/ClientRipple";
+import RecentRecordsPanel from "@/components/recent-records/RecentRecordsPanel";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -50,6 +51,7 @@ export default function RootLayout({
                     {children}
                   </DataLoader>
                 </ClientRipple>
+                <RecentRecordsPanel />
               </SyncProvider>
             </AuthProvider>
           </DataProvider>
